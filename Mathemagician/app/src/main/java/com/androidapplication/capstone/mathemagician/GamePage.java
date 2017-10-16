30880506feb192343a57483d6d54d17b20f51cde
jloures <john_loures@hotmail.com>
2017-10-16 19:50:35 -0400

    Added game page

---
+++ Mathemagician/app/src/main/java/com/androidapplication/capstone/mathemagician/GamePage.java
@@ -0,0 +1,20 @@
+package com.androidapplication.capstone.mathemagician;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class GamePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_page);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+}
--- Mathemagician/app/src/main/java/com/androidapplication/capstone/mathemagician/MainPage.java
+++ Mathemagician/app/src/main/java/com/androidapplication/capstone/mathemagician/MainPage.java
@@ -1,15 +1,12 @@
 package com.androidapplication.capstone.mathemagician;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    public void startGame(View view) {
+        Intent intent = new Intent(this, GamePage.class);
+        startActivity(intent);
     }
 }
